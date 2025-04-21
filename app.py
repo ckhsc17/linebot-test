0b282d9aa7fb9c3945dfabff6ebc0110c1e304e9
陳泊華 <94750792+ckhsc17@users.noreply.github.com>
2025-04-21 21:36:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,17 +33,18 @@ line_bot_api.push_message('Ub8de59324e70133461f9788aec4e68d9', TextSendMessage(t
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    #signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    print("Request body: " + body)
+    #app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    #try:
+    #    handler.handle(body, signature)
+    #except InvalidSignatureError:
+    #    abort(400)
 
     return 'OK'
 
